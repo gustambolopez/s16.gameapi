4f21575f603595641dd52876732d1b94524e67fa
gustambolopez <mogolixzq@gmail.com>
2025-07-15 12:43:16 -0500

    Update value.js

--- value.js
+++ value.js
@@ -1,4 +1,4 @@
 export const API = "https://html5-portal-api.gamedistribution.com/graphql";
-export const BASE = "https://s16assets.vercel.app/id/";
+export const BASE = "https://html5.gamedistribution.com/rvvASMiM/";
 export const linksuffix = "/index.html?gd_sdk_referrer_url=yjgames.gamedistribution.com";
 export const Image = "https://s16img.vercel.app/";
