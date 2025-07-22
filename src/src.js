782da41f9f4478957faeebe377b79c6f5ec93e8f
gustambolopez <mogolixzq@gmail.com>
2025-07-22 12:44:19 -0500

    Update src.js

--- src/src.js
+++ src/src.js
@@ -18,6 +18,13 @@ const cacheduration = 6 * 60 * 1000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  next();
+});
+
 import { api_graphql, link_base, linksuffix, Image } from './value.js';
 
 
