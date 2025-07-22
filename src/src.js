edfaa92cfff52ea1ca2555f273e95eaf6c95fc42
gustambolopez <mogolixzq@gmail.com>
2025-07-22 13:28:07 -0500

    fix cors issues

--- src/src.js
+++ src/src.js
@@ -20,7 +20,7 @@ app.use(express.json());
 // cors headers
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS'); 
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
   next();
 });
