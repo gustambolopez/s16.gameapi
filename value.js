2c705053cdd813a321d5885e07e2174464bbb98a
gustambolopez <mogolixzq@gmail.com>
2025-07-28 19:41:11 -0500

    Update value.js

--- value.js
+++ value.js
@@ -1,4 +1,4 @@
 export const api_graphql = "https://html5-portal-api.gamedistribution.com/graphql";
-export const link_base = "https://games.s16.lol/";
+export const link_base = "html5.gamedistribution.com/rvvASMiM/";
 export const linksuffix = "/index.html";
 export const Image = "https://img.s16.lol/";
